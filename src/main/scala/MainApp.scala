86326412c9d612740e014d01452f00ab6abf1c36
Vera Kornyukhina <kornuhina2005@mail.ru>
2025-05-11 01:23:19 +0300

    Update MainApp.scala

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -9,32 +9,25 @@ object MainApp {
       .master("local[*]")
       .getOrCreate()
 
-    // Инициализация платформы
     val dataMesh = new DataMeshPlatform(spark)
 
-    // Создаем тестового пользователя
     val analyst = User("user1", List("analytics"), List("analytics-team"))
     val salesUser = User("user2", List("sales"), List("sales-team"))
 
-    // Поиск данных через каталог - теперь доступ через dataMesh.catalog
     val searchResults = dataMesh.catalog.searchDataProducts("sales")
     searchResults.show()
 
-    // Получение данных с проверкой доступа
     dataMesh.getDataProduct("sales", analyst).foreach(_.show())
     dataMesh.getDataProduct("customer", salesUser).foreach(_.show())
 
-    // Запуск потоковой обработки с мониторингом качества
     val kafkaProduct = new EnhancedKafkaDataProduct(
       spark,
       "localhost:9092",
       dataMesh.qualityMonitor // Теперь доступ через dataMesh.qualityMonitor
     )
 
-    // Сложный сценарий: объединение потоков
     val joinedStream = kafkaProduct.joinStreams("sales", "customers", "customer_id")
 
-    // Запуск агрегаций с состоянием
     val salesAggregations = kafkaProduct.aggregateWithState(
       "sales",
       "product_id",
